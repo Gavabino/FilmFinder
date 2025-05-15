f30c40656273dc52dcf5374ecb36201a3fbe257f
Gavin Atkinson <gavinatkinson08@gmail.com>
2025-05-14 20:43:18 -0500

    Fixed Tailwind issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: false,
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
